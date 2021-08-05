b03e98bb6bd86c0eb02b06f07c76c47f33d81032
Ben Rosen <benjamin.j.rosen@gmail.com>
2021-08-05 15:29:16 -0400

    break up discussions in output

--- penn_canvas/archive.py
+++ penn_canvas/archive.py
@@ -59,11 +59,22 @@ def get_discussions(course_id, instance):
 
 
 def archive_main(course_id, instance, verbose, force):
-    def archive_discussion(discussion, verbose=False):
+    def archive_discussion(discussion, verbose=False, index=0, total=0):
         discussion_path = RESULTS / f"{discussion.title.strip().replace(' ', '_')}.csv"
         entries = discussion.get_topic_entries()
+        total_entries = 0
 
-        def process_entry(entry, verbose):
+        for entry in entries:
+            total_entries += 1
+
+        def process_entry(
+            entry,
+            verbose,
+            discussion_index,
+            total_discussions,
+            entry_index,
+            total_entries,
+        ):
             user = " ".join(entry.user["display_name"].split())
             timestamp = entry.created_at
             message = strip_tags(entry.message.replace("\n", " ")).strip()
@@ -72,13 +83,22 @@ def archive_main(course_id, instance, verbose, force):
                 user_display = colorize(user, "cyan")
                 timestamp_display = colorize(timestamp, "yellow")
                 discussion_display = colorize(discussion.title.upper(), "magenta")
+
+                if entry_index == 0:
+                    echo(f"==== DISCUSSION {discussion_index + 1} ====")
+
                 echo(
-                    f"- {discussion_display} {user_display} {timestamp_display} {message[:88]}"
+                    f"- [{discussion_index + 1}/{total_discussions}]"
+                    f" ({entry_index + 1}/{total_entries})"
+                    f" {discussion_display} {user_display} {timestamp_display} {message[:88]}"
                 )
 
             return [user, timestamp, message]
 
-        entries = [process_entry(entry, verbose) for entry in entries]
+        entries = [
+            process_entry(entry, verbose, index, total, entry_index, total_entries)
+            for entry_index, entry in enumerate(entries)
+        ]
         entries = DataFrame(entries, columns=["user", "timestamp", "post"])
         entries.to_csv(discussion_path, index=False)
 
@@ -93,8 +113,8 @@ def archive_main(course_id, instance, verbose, force):
     echo(") Processing discussions...")
 
     if verbose:
-        for discussion in discussions:
-            archive_discussion(discussion, True)
+        for index, discussion in enumerate(discussions):
+            archive_discussion(discussion, True, index, total)
     else:
         with progressbar(discussions, length=total) as progress:
             for discussion in progress:
