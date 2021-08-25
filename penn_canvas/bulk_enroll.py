f3d830dcd77c6073d114701d1c6e81025474029c
Ben Rosen <benrosen@upenn.edu>
2021-08-25 16:42:12 -0400

    use lazy loading of canvasapi

--- penn_canvas/bulk_enroll.py
+++ penn_canvas/bulk_enroll.py
@@ -24,6 +24,7 @@ ONGOING_TERM_ID = 4373
 def get_tomorrow():
     current_time = datetime.utcnow()
     now_plus_one_day = current_time + timedelta(days=1)
+
     return now_plus_one_day.isoformat() + "Z"
 
 
@@ -53,7 +54,7 @@ def bulk_enroll_main(user, sub_account, terms, input_file, dry_run, test):
     CANVAS = get_canvas(INSTANCE)
     ACCOUNT = CANVAS.get_account(MAIN_ACCOUNT_ID)
     SUB_ACCOUNT = CANVAS.get_account(sub_account)
-    NOW = get_tomorrow()
+    TOMORROW = get_tomorrow()
 
     try:
         USER_NAME = CANVAS.get_user(user)
@@ -64,19 +65,18 @@ def bulk_enroll_main(user, sub_account, terms, input_file, dry_run, test):
 
     echo(f") Finding {SUB_ACCOUNT} courses for {TOTAL} terms...")
 
-    COURSES = list()
-
-    for term in terms:
-        COURSES.extend(
-            [
-                course
-                for course in ACCOUNT.get_courses(
-                    enrollment_term_id=term, by_subaccounts=[sub_account]
-                )
-            ]
-        )
-
     if dry_run:
+        COURSES = list()
+
+        for term in terms:
+            COURSES.extend(
+                [
+                    course
+                    for course in ACCOUNT.get_courses(
+                        enrollment_term_id=term, by_subaccounts=[sub_account]
+                    )
+                ]
+            )
         course_codes = [
             course.sis_course_id if course.sis_course_id else course.name
             for course in COURSES
@@ -98,45 +98,51 @@ def bulk_enroll_main(user, sub_account, terms, input_file, dry_run, test):
 
         echo(f") Enrolling {USER_NAME} in {SUB_ACCOUNT} courses...")
 
-        for course in COURSES:
-            try:
-                enrollment_term_id = course.enrollment_term_id
-                end_at = ""
-                course.update(course={"term_id": ONGOING_TERM_ID})
+        for term in terms:
+            for course in ACCOUNT.get_courses(
+                enrollment_term_id=term, by_subaccounts=[sub_account]
+            ):
+                try:
+                    enrollment_term_id = course.enrollment_term_id
+                    end_at = ""
+                    course.update(course={"term_id": ONGOING_TERM_ID})
 
-                if course.end_at:
-                    end_at = course.end_at
-                    course.update(course={"end_at": NOW})
+                    if course.end_at:
+                        end_at = course.end_at
+                        course.update(course={"end_at": TOMORROW})
 
-                enrollment = course.enroll_user(
-                    user, enrollment={"enrollment_state": "active"}
-                )
-                course.update(course={"term_id": enrollment_term_id})
+                    enrollment = course.enroll_user(
+                        user, enrollment={"enrollment_state": "active"}
+                    )
+                    course.update(course={"term_id": enrollment_term_id})
 
-                if end_at:
-                    course.update(course={"end_at": end_at})
+                    if end_at:
+                        course.update(course={"end_at": end_at})
 
-                echo(f"- ENROLLED {USER_NAME} in {course.name}: {enrollment}")
-            except Exception as error:
-                colorize(
-                    f"- ERROR: Failed to enroll {USER_NAME} in {course.name} ({error})",
-                    "red",
-                    True,
-                )
-
-                with open(ERROR_FILE, "a+", newline="") as error_file:
-                    errors = set(read_csv(ERROR_FILE)["canvas sis course id"])
-                    course_log = (
-                        course.sis_course_id if course.sis_course_id else course.name
+                    echo(f"- ENROLLED {USER_NAME} in {course.name}: {enrollment}")
+                except Exception as error:
+                    colorize(
+                        f"- ERROR: Failed to enroll {USER_NAME} in"
+                        f" {course.name} ({error})",
+                        "red",
+                        True,
                     )
 
-                    if course_log not in errors:
-                        writer(error_file).writerow(
-                            [
-                                course_log,
-                                course.id,
-                                error,
-                            ]
+                    with open(ERROR_FILE, "a+", newline="") as error_file:
+                        errors = set(read_csv(ERROR_FILE)["canvas sis course id"])
+                        course_log = (
+                            course.sis_course_id
+                            if course.sis_course_id
+                            else course.name
                         )
+
+                        if course_log not in errors:
+                            writer(error_file).writerow(
+                                [
+                                    course_log,
+                                    course.id,
+                                    error,
+                                ]
+                            )
 
     colorize("FINISHED", "yellow", True)
