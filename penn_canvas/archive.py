f269afbb7ea9bd66c29d94059bb2d7d523ca4cee
Ben Rosen <benrosen@upenn.edu>
2021-11-23 11:47:38 -0500

    add assignments and discussion message

--- penn_canvas/archive.py
+++ penn_canvas/archive.py
@@ -53,60 +53,133 @@ def get_quizzes(course):
     return quizzes, len(quizzes)
 
 
+def process_submission(
+    submission,
+    verbose,
+    assignment_index,
+    total_assignments,
+    submission_index,
+    total_submissions,
+    assignment,
+    canvas,
+):
+    canvas_user = canvas.get_user(submission.user_id)
+    if verbose:
+        user_display = color(canvas_user, "cyan")
+        assignment_display = color(assignment, "magenta")
+        if submission_index == 0:
+            echo(f"==== ASSIGNMENT {assignment_index + 1} ====")
+        echo(
+            f"- [{assignment_index + 1}/{total_assignments}]"
+            f" ({submission_index + 1}/{total_submissions}) {assignment_display}"
+            f" {user_display}"
+            f" {submission.grade}..."
+            f" {submission.body}..."
+        )
+
+    return [canvas_user.name, assignment, submission.grade, submission.body]
+
+
+def process_entry(
+    entry,
+    verbose,
+    discussion_index,
+    total_discussions,
+    entry_index,
+    total_entries,
+    discussion,
+    canvas,
+    use_timestamp,
+):
+    user = " ".join(entry.user["display_name"].split())
+    user_id = entry.user["id"]
+    canvas_user = canvas.get_user(user_id)
+    email = canvas_user.email
+    message = " ".join(strip_tags(entry.message.replace("\n", " ")).strip().split())
+    timestamp = (
+        datetime.strptime(entry.created_at, "%Y-%m-%dT%H:%M:%SZ").strftime(
+            "%m/%d/%Y, %H:%M:%S"
+        )
+        if use_timestamp
+        else ""
+    )
+    if verbose:
+        user_display = color(user, "cyan")
+        timestamp_display = color(timestamp, "yellow") if use_timestamp else ""
+        email_display = color(email, "yellow") if not use_timestamp else ""
+        discussion_display = color(discussion.title.upper(), "magenta")
+        if entry_index == 0:
+            echo(f"==== DISCUSSION {discussion_index + 1} ====")
+        echo(
+            f"- [{discussion_index + 1}/{total_discussions}]"
+            f" ({entry_index + 1}/{total_entries}) {discussion_display}"
+            f" {user_display}"
+            f" {timestamp_display if use_timestamp else email_display}"
+            f" {message[:40]}..."
+        )
+
+    return (
+        [user, email, timestamp, message] if use_timestamp else [user, email, message]
+    )
+
+
 def archive_main(course_id, instance, verbose, use_timestamp, exclude_quizzes):
-    def archive_discussion(discussion, verbose=False, index=0, total=0):
-        def process_entry(
-            entry,
-            verbose,
-            discussion_index,
-            total_discussions,
-            entry_index,
-            total_entries,
-        ):
-            user = " ".join(entry.user["display_name"].split())
-            user_id = entry.user["id"]
-            canvas_user = CANVAS.get_user(user_id)
-            email = canvas_user.email
-            message = " ".join(
-                strip_tags(entry.message.replace("\n", " ")).strip().split()
+    def archive_assignment(canvas, assignment, index, total):
+        submissions_path = (
+            ASSIGNMENT_DIRECTORY
+            / f"{assignment.name.replace(' ', '_').replace('/', '-')}_SUBMISSIONS.csv"
+        )
+        assignment_path = (
+            ASSIGNMENT_DIRECTORY
+            / f"{assignment.name.replace(' ', '_').replace('/', '-')}.md"
+        )
+        submissions = [submission for submission in assignment.get_submissions()]
+        submissions = [
+            process_submission(
+                submission,
+                verbose,
+                index,
+                total,
+                submission_index,
+                len(submissions),
+                assignment,
+                canvas,
             )
-            timestamp = (
-                datetime.strptime(entry.created_at, "%Y-%m-%dT%H:%M:%SZ").strftime(
-                    "%m/%d/%Y, %H:%M:%S"
-                )
-                if use_timestamp
-                else ""
+            for submission_index, submission in enumerate(submissions)
+        ]
+        columns = ["User", "Assignment", "Grade", "Body"]
+        submissions = DataFrame(submissions, columns=columns)
+        submissions.to_csv(submissions_path, index=False)
+        with open(assignment_path, "w") as assignment_file:
+            assignment_file.write(
+                assignment.description if assignment.description else ""
             )
-            if verbose:
-                user_display = color(user, "cyan")
-                timestamp_display = color(timestamp, "yellow") if use_timestamp else ""
-                email_display = color(email, "yellow") if not use_timestamp else ""
-                discussion_display = color(discussion.title.upper(), "magenta")
-                if entry_index == 0:
-                    echo(f"==== DISCUSSION {discussion_index + 1} ====")
-                echo(
-                    f"- [{discussion_index + 1}/{total_discussions}]"
-                    f" ({entry_index + 1}/{total_entries}) {discussion_display}"
-                    f" {user_display}"
-                    f" {timestamp_display if use_timestamp else email_display}"
-                    f" {message[:40]}..."
-                )
 
-            if use_timestamp:
-                return [user, email, timestamp, message]
-            else:
-                return [user, email, message]
-
+    def archive_discussion(canvas, discussion, verbose=False, index=0, total=0):
         discussion_path = (
             DISCUSSION_DIRECTORY
-            / f"{discussion.title.replace(' ', '_').replace('/', '-')}.csv"
+            / f"{discussion.title.replace(' ', '_').replace('/', '-')}_POSTS.csv"
+        )
+        description_path = (
+            DISCUSSION_DIRECTORY
+            / f"{discussion.title.replace(' ', '_').replace('/', '-')}.md"
         )
         entries = [entry for entry in discussion.get_topic_entries()]
         if verbose and not entries:
             echo(f"==== DISCUSSION {index + 1} ====")
             echo("- NO ENTRIES")
         entries = [
-            process_entry(entry, verbose, index, total, entry_index, len(entries))
+            process_entry(
+                entry,
+                verbose,
+                index,
+                total,
+                entry_index,
+                len(entries),
+                discussion,
+                canvas,
+                use_timestamp,
+            )
             for entry_index, entry in enumerate(entries)
         ]
         columns = ["User", "Email", "Timestamp", "Post"]
@@ -114,6 +187,8 @@ def archive_main(course_id, instance, verbose, use_timestamp, exclude_quizzes):
             columns.remove("Timestamp")
         entries = DataFrame(entries, columns=columns)
         entries.to_csv(discussion_path, index=False)
+        with open(description_path, "w") as description_file:
+            description_file.write(discussion.message if discussion.message else "")
 
     def archive_quizzes(quiz, verbose=False, quiz_index=0):
         quiz_path = (
@@ -136,14 +211,15 @@ def archive_main(course_id, instance, verbose, use_timestamp, exclude_quizzes):
 
     CANVAS = get_canvas(instance)
     course = CANVAS.get_course(course_id)
-    assignments, total = get_assignments(course)
-    discussions, total = get_discussions(course)
+    assignments, assignment_total = get_assignments(course)
+    discussions, discussion_total = get_discussions(course)
     quizzes = []
     quiz_total = 0
     COURSE = RESULTS / f"{course.name}"
     DISCUSSION_DIRECTORY = COURSE / "Discussions"
+    ASSIGNMENT_DIRECTORY = COURSE / "Assignments"
     QUIZ_DIRECTORY = COURSE / "Quizzes"
-    PATHS = [COURSE, DISCUSSION_DIRECTORY]
+    PATHS = [COURSE, DISCUSSION_DIRECTORY, ASSIGNMENT_DIRECTORY]
     if not exclude_quizzes:
         PATHS.append(QUIZ_DIRECTORY)
     for path in PATHS:
@@ -152,11 +228,13 @@ def archive_main(course_id, instance, verbose, use_timestamp, exclude_quizzes):
     echo(") Processing discussions...")
     if verbose:
         for index, discussion in enumerate(discussions):
-            archive_discussion(discussion, True, index, total)
+            archive_discussion(CANVAS, discussion, True, index, discussion_total)
+        for index, assignment in enumerate(assignments):
+            archive_assignment(CANVAS, assignment, index, assignment_total)
     else:
-        with progressbar(discussions, length=total) as progress:
+        with progressbar(discussions, length=discussion_total) as progress:
             for discussion in progress:
-                archive_discussion(discussion)
+                archive_discussion(CANVAS, discussion)
     if not exclude_quizzes:
         quizzes, quiz_total = get_quizzes(course)
         echo(") Processing quizzes...")
@@ -169,7 +247,7 @@ def archive_main(course_id, instance, verbose, use_timestamp, exclude_quizzes):
                     archive_quizzes(quiz)
     color("SUMMARY", "yellow", True)
     echo(
-        f"- Archived {color(total, 'magenta')} DISCUSSIONS for"
+        f"- Archived {color(discussion_total, 'magenta')} DISCUSSIONS for"
         f" {color(course.name, 'blue')}."
     )
     if not exclude_quizzes:
