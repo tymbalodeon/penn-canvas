55ceeab3b944cc88981517a9f5fe34ffed954a99
Ben Rosen <benrosen@upenn.edu>
2022-02-08 17:22:48 -0500

    add extra params to sandbox functions

--- sandbox.py
+++ sandbox.py
@@ -9,26 +9,28 @@ from penn_canvas.helpers import MAIN_ACCOUNT_ID, get_canvas
 pretty_printer = PrettyPrinter()
 
 
-def get_account(account, instance="prod"):
-    account = get_canvas(instance).get_account(account if account else MAIN_ACCOUNT_ID)
+def get_account(account, use_sis_id=False, instance="prod"):
+    account = get_canvas(instance).get_account(
+        account if account else MAIN_ACCOUNT_ID, use_sis_id=use_sis_id
+    )
     pretty_printer.pprint(vars(account))
     return account
 
 
-def get_user(user, instance="prod"):
-    user = get_canvas(instance).get_user(user)
+def get_user(user, id_type=None, instance="prod"):
+    user = get_canvas(instance).get_user(user, id_type=id_type)
     pretty_printer.pprint(vars(user))
     return user
 
 
-def get_course(course, instance="prod"):
-    course = get_canvas(instance).get_course(course)
+def get_course(course, use_sis_id=False, instance="prod"):
+    course = get_canvas(instance).get_course(course, use_sis_id=use_sis_id)
     pretty_printer.pprint(vars(course))
     return course
 
 
-def get_section(section, instance="prod"):
-    section = get_canvas(instance).get_section(section)
+def get_section(section, use_sis_id=False, instance="prod"):
+    section = get_canvas(instance).get_section(section, use_sis_id=use_sis_id)
     pretty_printer.pprint(vars(section))
     return section
 
