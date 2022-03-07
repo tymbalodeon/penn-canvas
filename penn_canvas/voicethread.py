e716953dd6092dec8bbb443a510214ce1e9f3892
Ben Rosen <benrosen@upenn.edu>
2022-03-07 10:38:11 -0500

    add turnitin to voicethread

--- penn_canvas/voicethread.py
+++ penn_canvas/voicethread.py
@@ -3,14 +3,24 @@ from typer import echo
 from .helpers import MAIN_ACCOUNT_ID, color, get_canvas, get_command_paths
 
 
-def is_voicethread_assignments(assignment):
+def is_turnitin_assignment(assignment):
+    try:
+        if "url" in assignment.external_tool_tag_attributes:
+            return "turnitin" in assignment.external_tool_tag_attributes["url"]
+        else:
+            return False
+    except Exception:
+        return False
+
+
+def is_voicethread_assignment(assignment):
     try:
         return "voicethread" in assignment.external_tool_tag_attributes["url"]
     except Exception:
         return False
 
 
-COMMAND = "Count Voicethread"
+COMMAND = "Count Tool Usage"
 RESULTS = get_command_paths(COMMAND, no_input=True)[0]
 HEADERS = [
     "canvas course id",
@@ -19,7 +29,6 @@ HEADERS = [
     "account",
     "term",
     "status",
-    "voicethread assignments",
 ]
 terms = ["A", "B", "C"]
 year_and_terms = [f"2021{term}" for term in terms]
@@ -40,25 +49,44 @@ def get_courses(term, main_account, account):
     ]
 
 
-def voicethread_main():
+def voicethread_main(tool):
     canvas = get_canvas()
     main_account = canvas.get_account(MAIN_ACCOUNT_ID)
     account = canvas.get_account(account_id)
     for term in year_and_terms:
-        results_path = RESULTS / f"{term}_voicethread_usage.csv"
+        results_path = RESULTS / f"{term}_{tool}_usage.csv"
+        if tool == "turnitin":
+            headers = HEADERS + [
+                "turnitin assignments",
+                "published turnitin assignemnts",
+            ]
+        else:
+            headers = HEADERS + ["voicethread assignments"]
         if not results_path.exists():
             with open(results_path, "w") as results_file:
-                results_file.write(",".join(HEADERS))
+                results_file.write(",".join(headers))
         courses = get_courses(term, main_account, account)
         courses_count = len(courses)
         for index, course in enumerate(courses):
             assignments = [assignment for assignment in course.get_assignments()]
-            assignments = [
-                assignment
-                for assignment in assignments
-                if is_voicethread_assignments(assignment)
-            ]
-            voicethread_count = len(assignments)
+            published_count = ""
+            if tool == "turnitin":
+                assignments = [
+                    assignment
+                    for assignment in assignments
+                    if is_turnitin_assignment(assignment)
+                ]
+                published = [
+                    assignment for assignment in assignments if assignment.published
+                ]
+                published_count = len(published)
+            else:
+                assignments = [
+                    assignment
+                    for assignment in assignments
+                    if is_voicethread_assignment(assignment)
+                ]
+            count = len(assignments)
             account_object = canvas.get_account(course.account_id)
             row = [
                 course.id,
@@ -67,14 +95,16 @@ def voicethread_main():
                 account_object,
                 term,
                 course.workflow_state,
-                voicethread_count,
+                count,
             ]
+            if tool == "turnitin":
+                row.append(published_count)
             row = [str(item).replace(",", "-") for item in row]
             with open(results_path, "a") as results_file:
                 results_file.write("\n")
                 results_file.write(",".join(row))
-            display_color = "green" if voicethread_count else "yellow"
+            display_color = "green" if count else "yellow"
             echo(
                 f" - ({index + 1}/{courses_count}) {color(course.name)}:"
-                f" {color(voicethread_count, display_color)}"
+                f" {color(count, display_color)}"
             )
