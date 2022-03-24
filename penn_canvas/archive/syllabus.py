42de6ae8fdd77cf1bd8a5f3098e19890961200bf
Ben Rosen <benrosen@upenn.edu>
2022-03-24 09:18:43 -0400

    fix syllabus

--- penn_canvas/archive/syllabus.py
+++ penn_canvas/archive/syllabus.py
@@ -11,8 +11,9 @@ from .helpers import strip_tags
 def archive_syllabus(course: Course, course_path: Path, verbose: bool):
     echo(") Exporting syllabus...")
     syllabus_path = create_directory(course_path / "Syllabus")
-    syllabus = strip_tags(course.syllabus_body)
+    syllabus = course.syllabus_body
     if syllabus:
+        syllabus = strip_tags(syllabus)
         with open(syllabus_path / "syllabus.txt", "w") as syllabus_file:
             syllabus_file.write(syllabus)
         if verbose:
